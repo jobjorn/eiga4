df78dd95b185d7e4429f40cce500728fe5e5d019
Hedvig Mejstedt <hedvig@mejstedt.se>
2024-03-18 21:40:23 +0100

    optimistic updates

--- components/NamesForm.tsx
+++ components/NamesForm.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useOptimistic, useState } from 'react';
 import {
   Alert,
   Box,
@@ -12,9 +12,23 @@ import { addNames } from 'app/actions';
 import { useFormState } from 'react-dom';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { Submit } from './Submit';
+import { ListWithNames } from 'types/types';
+import { theme } from 'styles/theme';
+import { NamesList } from './NamesList';
 
-export const NamesForm: React.FC<{}> = () => {
+type Props = {
+  list: ListWithNames[];
+};
+
+export const NamesForm: React.FC<{}> = ({ list }: Props) => {
   const { user, isLoading } = useUser();
+  const [textField, setTextField] = useState('');
+  const [optimisticNameList, addOptimisticNameList] = useOptimistic(
+    list,
+    (state, newNames: ListWithNames[]) => {
+      return [...state, newNames];
+    }
+  );
 
   const addNamesWithId = addNames.bind(null, user?.sub);
 
@@ -30,40 +44,92 @@ export const NamesForm: React.FC<{}> = () => {
 
   if (isLoading) {
     return (
-      <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '5px'
+        }}
+      >
         <Typography variant="h5">Lägg till nya namn</Typography>
-        <Skeleton variant="rectangular" height={60} />;
-      </>
+        <Skeleton variant="rectangular" height={60} />
+      </div>
     );
   }
 
   if (!user || !user.sub) {
     return (
-      <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '5px'
+        }}
+      >
         <Typography variant="h5">Lägg till nya namn</Typography>
         <Box mt={2}>
           <Alert severity="error">Ej inloggad!</Alert>
         </Box>
-      </>
+      </div>
     );
   }
 
   return (
     <>
-      <Typography variant="h5">Lägg till nya namn</Typography>
-      {statusMessage && (
-        <Box mt={2}>
-          <Alert severity={statusMessage.severity}>
-            {statusMessage.message}
-          </Alert>
-        </Box>
-      )}
-      <form action={formAction} ref={formElement}>
-        <Stack spacing={2}>
-          <TextField label="Namn" name="names" multiline minRows={3} />
-        </Stack>
-        <Submit>Lägg till</Submit>
-      </form>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '5px'
+        }}
+      >
+        <Typography variant="h5">Lägg till nya namn</Typography>
+        {statusMessage && (
+          <Box mt={2}>
+            <Alert severity={statusMessage.severity}>
+              {statusMessage.message}
+            </Alert>
+          </Box>
+        )}
+        <form
+          action={async (formData) => {
+            const namesString = formData.get('names') as string;
+            const namesArray = namesString
+              .split(/[\n,]/)
+              .map((name) => name.trim());
+
+            const convertToNameList = (namesArray) => {
+              const X = {
+                id: null,
+                name: { name: namesArray[0] },
+                nameId: null,
+                subArray: null,
+                userId: user.sub
+              };
+              return X;
+            };
+
+            addOptimisticNameList(convertToNameList(namesArray));
+            await formAction(formData);
+          }}
+          ref={formElement}
+        >
+          <Stack spacing={2}>
+            <TextField
+              label="Namn"
+              name="names"
+              multiline
+              minRows={3}
+              onChange={(event) => setTextField(event.target.value)}
+            />
+            <Submit disabled={textField === ''}>Lägg till</Submit>
+          </Stack>
+        </form>
+      </div>
+      <NamesList list={optimisticNameList} />
+      <div
+        style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}
+      ></div>
     </>
   );
 };
--- components/Submit.tsx
+++ components/Submit.tsx
@@ -2,13 +2,14 @@ import { useFormStatus } from 'react-dom';
 import { Button } from '@mui/material';
 import React from 'react';
 
-export const Submit: React.FC<{ children: React.ReactNode }> = ({
-  children
-}) => {
+export const Submit: React.FC<{
+  children: React.ReactNode;
+  disabled: boolean;
+}> = ({ children, disabled }) => {
   const { pending } = useFormStatus();
 
   return (
-    <Button variant="contained" type="submit" disabled={pending}>
+    <Button variant="contained" type="submit" disabled={pending || disabled}>
       {children}
     </Button>
   );
