c815561e8aca568fc11d993498a725891a79680e
Hedvig Mejstedt <hedvig@mejstedt.se>
2024-04-16 21:16:27 +0200

    styling nameList component

--- components/NamesList.tsx
+++ components/NamesList.tsx
@@ -6,14 +6,15 @@ import { ListWithNames } from 'types/types';
 import { Submit } from './Submit';
 import { useFormState } from 'react-dom';
 import { theme } from 'styles/theme';
+import { AiOutlineClose } from 'react-icons/ai';
 
 type NamesListProps = {
   list: ListWithNames[];
+  user: any;
 };
 export const NamesList = (props: NamesListProps) => {
   const formElement = useRef<HTMLFormElement>(null);
   const [statusMessage, formAction] = useFormState(removeName, null);
-
   /*
   förslag: använd 1 form för hela listan och skicka med id:t
   i submit-knappen istället för som hidden input (submit name="remove" value={id})
@@ -28,8 +29,8 @@ export const NamesList = (props: NamesListProps) => {
     <div
       style={{
         display: 'grid',
-        gap: '5px',
-        gridTemplateColumns: '170px 170px 170px'
+        gridTemplateColumns: 'repeat(auto-fill, minmax(170px, 1fr))',
+        gap: '5px'
       }}
     >
       {props.list.map((item) => {
@@ -42,13 +43,21 @@ export const NamesList = (props: NamesListProps) => {
               display: `flex`,
               justifyContent: `space-between`,
               alignItems: `center`,
-              padding: '3px'
+              padding: '5px'
             }}
           >
-            <Typography variant="body1">{item.name.name}</Typography>
+            <div style={{ display: 'flex', gap: '5px' }}>
+              <img
+                src={item.avatar}
+                style={{ width: '20px', height: '20px', borderRadius: '50%' }}
+              />
+              <Typography variant="body1">{item.name}</Typography>
+            </div>
             <form action={formAction} ref={formElement}>
               <input type="hidden" name="id" value={item.id} />
-              <Submit>X</Submit>
+              <Submit variant="text">
+                <AiOutlineClose />
+              </Submit>
             </form>
           </div>
         );
