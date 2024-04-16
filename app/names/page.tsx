c405fe64e7dab1ad2d055e13a69518b3482c126c
Hedvig Mejstedt <hedvig@mejstedt.se>
2024-04-16 21:15:40 +0200

    remove todos

--- app/names/page.tsx
+++ app/names/page.tsx
@@ -6,25 +6,15 @@ import { ListWithNames } from 'types/types';
 export default async function Page() {
   const list: ListWithNames[] = await getNameList();
   return (
-    <>
-      <ul>
-        <li>
-          Olika färger beroende på vem som lagt till namnet (du, din partner,
-          båda)?
-        </li>
-        <li>Ett kryss istället för "Remove knappen"</li>
-        <li>Mer kompakt, kolumner (beroende på sidans bredd) (css columns)</li>
-      </ul>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '20px'
-        }}
-      >
-        <Typography variant="h3">Namn</Typography>
-        <NamesForm list={list} />
-      </div>
-    </>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '20px'
+      }}
+    >
+      <Typography variant="h3">Namn</Typography>
+      <NamesForm list={list} />
+    </div>
   );
 }
