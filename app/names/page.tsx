6071e18ac480642a9216b7c3ee40917f4b6f54a9
Jobjörn Folkesson <jobjorn@jobjorn.se>
2024-05-28 22:48:11 +0200

    null check på getUserWithPartners-hämtningen

--- app/names/page.tsx
+++ app/names/page.tsx
@@ -1,14 +1,18 @@
-import { Box } from '@mui/material';
+import { Box, Skeleton } from '@mui/material';
 import { getNameList } from 'app/actions';
 import { getUserWithPartners } from 'app/overview/actions';
 import { PageTitle } from 'app/uicomponents/PageTitle';
 import { NamesForm } from 'components/NamesForm';
 import { VotingInvitation } from 'components/VotingInvitation';
-import { ListWithNames } from 'types/types';
+import { ListWithNames, UserWithPartners } from 'types/types';
 
 export default async function Page() {
   const list: ListWithNames[] = await getNameList();
-  const userWithPartners = await getUserWithPartners();
+  const userWithPartners: UserWithPartners | null = await getUserWithPartners();
+
+  if (!userWithPartners) {
+    return <Skeleton></Skeleton>;
+  }
 
   return (
     <>
