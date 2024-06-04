04bbd05a4e90448ac60c3c519fd5fc9aef5e56f4
Jobjörn Folkesson <jobjorn@jobjorn.se>
2024-06-04 20:00:53 +0200

    strukturerar om så att det går att gå vidare utan partner

--- components/VotingInvitation.tsx
+++ components/VotingInvitation.tsx
@@ -13,88 +13,19 @@ export const VotingInvitation: React.FC<{
   user: UserWithPartners;
   hasPartner: boolean;
 }> = ({ user, hasPartner }) => {
-  const [statusMessageStartVote, formActionStartVote] = useFormState(
-    startVoting,
-    null
-  );
-
-  /* 
-  1. Kan man påbörja omröstning som ensam förälder?? 
-  */
-
+  const [statusMessage, formActionStartVote] = useFormState(startVoting, null);
   const formElement = useRef<HTMLFormElement>(null);
+
   if (user === null) {
     return (
       <PartnerBox>
         <Skeleton style={{ flexGrow: 1 }} />
       </PartnerBox>
     );
-  } else if (
-    (user.readyToVote === false && user?.partnered.length === 0) ||
-    (user.readyToVote === false &&
-      user.partnering[0].partnering.readyToVote === false)
-  ) {
-    // Om användaren inte har någon definierad partner och inte heller någon inbjudan till att bli partnerad
-    // FIXME: Ovan beskrivning motsvarar inte if-satsen
-    return (
-      <form ref={formElement} action={formActionStartVote}>
-        <Box
-          sx={{
-            display: 'flex',
-            placeContent: 'end'
-          }}
-        >
-          <Submit>Start Voting!</Submit>
-        </Box>
-
-        {statusMessageStartVote && (
-          <Box mt={2}>
-            <Alert severity={statusMessageStartVote.severity}>
-              {statusMessageStartVote.message}
-            </Alert>
-          </Box>
-        )}
-      </form>
-    );
   }
-  if (user.readyToVote && user.partnering[0].partnered?.readyToVote === false) {
-    // Om användaren är redo att rösta, men partnern inte är det
 
-    return (
-      <PartnerBox>
-        <Typography variant="body1">
-          Inväntar att {user.partnering[0].partnered?.name} blir redo att
-          påbörja röstning.
-        </Typography>
-      </PartnerBox>
-    );
-  } else if (
-    user.readyToVote === false &&
-    user.partnering[0].partnered?.readyToVote === true
-  ) {
-    // Om användarens partner är redo att rösta men inte användaren själv
-    return (
-      <form ref={formElement} action={formActionStartVote}>
-        <PartnerBox>
-          <p style={{ flexGrow: 1 }}>
-            {user.partnered[0].partnering.name} är redo att rösta, är du?
-          </p>
-          <Submit>Ja</Submit>
-        </PartnerBox>
-        {statusMessageStartVote && (
-          <Box mt={2}>
-            <Alert severity={statusMessageStartVote.severity}>
-              {statusMessageStartVote.message}
-            </Alert>
-          </Box>
-        )}
-      </form>
-    );
-  } else if (
-    user.readyToVote === true &&
-    user.partnering[0].partnered?.readyToVote === true
-  ) {
-    // Om användaren & partnern är redo att rösta
+  if (!hasPartner) {
+    // Ingen partner -  bara att börja rösta
     return (
       <Button
         LinkComponent={Link}
@@ -108,6 +39,61 @@ export const VotingInvitation: React.FC<{
         Till omröstning
       </Button>
     );
+  } else {
+    // Om det finns en partner
+    if (user.readyToVote === false) {
+      // Om den inloggade användaren inte är redo att börja rösta, visa redo-knapp:
+
+      return (
+        <form ref={formElement} action={formActionStartVote}>
+          <Box
+            sx={{
+              display: 'flex',
+              placeContent: 'end'
+            }}
+          >
+            <Submit>Redo att börja rösta</Submit>
+          </Box>
+
+          {statusMessage && (
+            <Box mt={2}>
+              <Alert severity={statusMessage.severity}>
+                {statusMessage.message}
+              </Alert>
+            </Box>
+          )}
+        </form>
+      );
+    } else {
+      // Om användaren är redo att rösta
+      if (user.partnering[0].partnered?.readyToVote === false) {
+        // Om partnern inte är redo att rösta
+        return (
+          <PartnerBox>
+            <Typography variant="body1">
+              Inväntar att {user.partnering[0].partnered?.name} blir redo att
+              påbörja röstning.
+            </Typography>
+          </PartnerBox>
+        );
+      } else if (user.partnering[0].partnered?.readyToVote === true) {
+        // Om partnern är redo att rösta
+        return (
+          <Button
+            LinkComponent={Link}
+            style={{ alignSelf: 'flex-end' }}
+            href="/voting"
+            size="large"
+            variant="text"
+            color="secondary"
+            endIcon={<ArrowForwardIosIcon />}
+          >
+            Till omröstning
+          </Button>
+        );
+      } else {
+        return <Box>Okänt fel</Box>;
+      }
+    }
   }
-  return <Box>Okänt fel</Box>;
 };
